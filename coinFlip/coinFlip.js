e29ffaa53cd0a10b3b4b6fd35be9ae175f57cba1
Sean Yim <superjjang89@gmail.com>
2019-09-05 12:08:59 -0400

    coinflip app

---
+++ coinFlip/coinFlip.js
@@ -0,0 +1,59 @@
+class CoinFlip extends React.Component{
+    //this is a child, or subclass
+    //a child of React.Component
+    //React.Component is the parent or superclass
+    //we get cool stuff in CoinFlip because it is a subclass
+    constructor(){
+        super(); //call super because this is a childclass
+        this.heads = "buffalo-nickel-heads.jpg"
+        this.tails = "buffalo-nickel-tails.png"
+        //react does not care about heads, tails, or coin, at all
+        //that is regular JS
+        this.coin = [
+            this.heads,
+            this.tails
+        ]
+        //React cares about state. a lot. React is special
+        this.state = {
+            image: this.coin[0]
+        }
+        this.flip = this.flip.bind(this) // the ugly way
+    }
+  ///////////////////////////////////////////////  
+    // flip = () =>{
+
+    // }
+    flip(){
+        console.log(this);
+        //listen.
+        // console.log("Flip was CLicked, facts!");
+        //never change state yourself, ever
+        //React gave you a setter
+        let coinSide = Math.round(Math.random()); //0,1
+        this.setState({
+            image: this.coin[coinSide]
+        })
+    }
+
+    render(){
+
+        //it is render's job to return a single DOM element
+        //whether you like it or not, render will run right after the constructor.
+        //react events are identical (mostly) to regular DOM events,
+        //we attach them to the element and use camelCase.
+        //we hand them CODE
+        return(
+            <div className="coin-flip">
+                <button className="waves-effect waves-light btn" onClick={this.flip}>Flip!</button>
+                <img src={this.state.image} />
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(
+    <CoinFlip />,
+    document.getElementById('root')
+)
--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,45 @@
 // putting a bunch of components in this file
+//App is uppercase, which means it's a component.
+//all components, without exception, must return a single DOM.
+function Counter(props){
+    return (
+        <div className="counter">
+            <div className="buttons">
+                <button className="minus waves-effect waves-light btn" >-</button>
+                <button className="plus waves-effect waves-light btn">+</button>
+            </div>
+            <div className="team-score">0</div>
+        </div>
+    )
+}
 
+function Team(props){
+    return(
+        <div className="team">
+            <div className="team-name">{props.name}</div>
+            <Counter />                        
+        </div>
+    )
+}
+
+
+
+function App(props){
+    return(
+        <div className="container">
+            <div className="row center">
+                <div className="title">
+                    <h1>{props.title}</h1>
+                </div>
+                <div className="teams">
+                    <Team name="Bears"/>
+                    <Team name="Packers"/>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
 
 
 
@@ -9,6 +49,6 @@
 
 
 ReactDOM.render(
-    <h1>Facks!</h1>,
+    <App title = "Opening NFL Game"/>,
     document.querySelector('#root')
 )
